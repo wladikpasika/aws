25d4686ee247898ca7dea6db40536f179c1c2bb9
vladikpasika <wladikpasika@gmail.com>
2017-12-19 18:57:07 +0200

    rep wthout webpack dep

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
     ],
     resolve: {
         alias: {
-            'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' для webpack 1
+            'vue$': 'vue/dist/vue.esm.js'
         }
     },
     devServer: {
